5df18eba764c39cfd16f6400e3e8304ea7c0cf27
Richard-Burd <rick.a.burd@gmail.com>
2022-05-10 19:43:49 -0700

    recipes render conditionally on slugs

--- pages/index.js
+++ pages/index.js
@@ -19,6 +19,48 @@ export async function getStaticProps() {
 }
 
 export default function Recipes({ recipes }) {
+  console.log(recipes[0].fields.slug);
+  return (
+    <div className="recipe-list">
+      {recipes.map(function(recipe){
+        if (!recipe.fields.slug.startsWith("cheesy")) {
+          return <RecipeCard key={recipe.sys.id} recipe={recipe} />
+        }
+      })}
+
+      <style jsx>{`
+        .recipe-list {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          grid-gap: 20px 60px;
+        }
+      `}</style>
+    </div>
+  )
+}
+
+/*
+import { createClient } from "contentful";
+import RecipeCard from "../components/RecipeCard";
+
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  });
+
+  const res = await client.getEntries({ content_type: "recipe" });
+
+  return {
+    props: {
+      recipes: res.items,
+      revalidate: 1,
+    }
+  }
+
+}
+
+export default function Recipes({ recipes }) {
   console.log(recipes);
   return (
     <div className="recipe-list">
@@ -36,3 +78,4 @@ export default function Recipes({ recipes }) {
     </div>
   )
 }
+*/
